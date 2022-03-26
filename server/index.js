f498ddadc59944815c11b43627046f9dd4428e92
adithyanandakummar <adithyanandakummar10@gmail.com>
2022-03-26 09:12:38 +0530

    updated server

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
 
 const app = express();
 
@@ -13,8 +14,9 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://admin:admin123@tracker.rnyiu.mongodb.net/Tracker?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://admin:admin123@tracker.rnyiu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
